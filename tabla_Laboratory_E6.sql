a27b0f52133dd0a51b3a740f97ae8b215ccfffdf
kairayary <kaira.abreu@gmail.com>
2023-03-28 00:05:29 -0300

    completación del ejercicio práctico laboratory

--- tabla_Laboratory_E6.sql
+++ tabla_Laboratory_E6.sql
@@ -58,8 +58,49 @@ select * from client;
 
 select * from article where price >100;
 
-select * from article where price>20 and price< 40;
+select * from article where price between 20 and  40;
 
 select * from article where price>40 and price< 60;
 select * from article where  price= 20 and stock>30;
 
+select * from article where price in (12,20,30);
+
+select * from article where price in (10,110);
+
+
+insert into client
+values ('Carlos','Mata', '21-14987000-8', 'Coco 635'),
+('Yohana','Rojo','27-37562664-0','Maipú 1532'),
+('Valentina','Lugo', '24-28887418-8','Moscú 3'),
+('Patricia','Perez','27-11111665-2','Delia 3201'),
+('Juana','Mendez','27-235822222-9','Belgrano 6525');
+
+update client set name = 'José' where id = 1;
+
+update client set name = 'Pablo', last_name = 'Fuentes', cuit='20-21053119-9' where id = 3;
+
+set SQL_SAFE_UPDATES=0;
+delete from client where last_name like'%Perez%';
+select * from client;
+
+delete from client where cuit like'%0';
+
+select price, price*0.2 as aumento, price + (price*0.2) as total from article where price<=50;
+
+select price, price*0.15 as aumento, price + (price*0.15) as total from article where price>50;
+
+select price, price*0.05 as descuento, price - (price*0.05) as total from article where price>=200;
+
+set SQL_SAFE_UPDATES=0;
+delete from article where stock=0;
+
+select*from article;
+
+select max(amount) from invoice;
+select min(amount) from invoice;
+
+select min(amount) from invoice where year(2000) and year(2008);
+
+select amount, count(*) as recuento from invoice group by amount;
+
+select amount, count(*) as recuento from invoice where amount>=35000 and amount<=40000 group by amount;
